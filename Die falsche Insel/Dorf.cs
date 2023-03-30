8d22e6bd680b52f5d3066c3456761a7256fae923
Fawe00 <Fawe1@gmx.net>
2023-03-30 13:51:07 +0200

    Erstellung Dorf

---
+++ Die falsche Insel/Dorf.cs
@@ -0,0 +1,42 @@
+﻿class Dorf
+{
+    private String _eingabe = null;
+    private readonly Verabreitung _verarbeitung;
+    private readonly Inventar _inventar;
+
+
+
+    public Dorf(Verabreitung verabreitung, Inventar inventar)
+    {
+        _verarbeitung = verabreitung;
+        _inventar = inventar;
+    }
+    public String DorfMain()
+    {
+        Console.WriteLine("Wikommen im Dorf des Handels");
+        while (true)
+        {
+            Console.WriteLine("Sie können hier folgen Sachen machen:");
+            Console.WriteLine("7. Reisen Wald");
+            Console.WriteLine("8. Reisen Burg");
+            Console.WriteLine("9. Reisen Wiese");
+            Console.WriteLine("0. Inverntar ausgeben lassen");
+            _eingabe = Console.ReadLine();
+            if ( _eingabe != null )
+            {
+                switch ( _eingabe )
+                {
+                    case "7":
+                        return "Wald";
+                    case "8":
+                        return "Burg";
+                    case "9":
+                        return "Wiese";
+                    case "0":
+                        _inventar.InventarAusagabe();
+                        break;
+                }
+            }
+        }
+    }
+}
