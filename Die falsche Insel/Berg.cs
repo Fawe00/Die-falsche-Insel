6a6e775a75004b1a9a54dbce455353a128dfe577
Fawe00 <Fawe1@gmx.net>
2023-03-31 12:04:36 +0200

    Erstellen Berg

---
+++ Die falsche Insel/Berg.cs
@@ -0,0 +1,40 @@
+﻿class Berg
+{
+    private String _eingabe = null;
+    private readonly Verabreitung _verarbeitung;
+    private readonly Inventar _inventar;
+
+
+
+    public Berg(Verabreitung verabreitung, Inventar inventar)
+    {
+        _verarbeitung = verabreitung;
+        _inventar = inventar;
+    }
+    public String BurgMain()
+    {
+        Console.WriteLine("Wikommen auf dem Berg");
+        while (true)
+        {
+            Console.WriteLine("Sie können hier folgen Sachen machen:");
+            Console.WriteLine("1. Stein abbauen");
+            Console.WriteLine("9. Reisen Wald");
+            Console.WriteLine("0. Inventar ausgeben lassen");
+            _eingabe = Console.ReadLine();
+            if (_eingabe != null)
+            {
+                switch (_eingabe)
+                {
+                    case "9":
+                        return "Wald";
+                    case "0":
+                        _inventar.InventarAusagabe();
+                        break;
+                    default:
+                        Console.WriteLine("Sie haben keine Nummer wo möglich ist.");
+                        break;
+                }
+            }
+        }
+    }
+}
