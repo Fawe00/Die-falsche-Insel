de66d5d51d6e3ec22f3c4867891a8aa24786259d
Fawe00 <Fawe1@gmx.net>
2023-03-31 12:16:48 +0200

    Stein Hinzugefügt

--- Die falsche Insel/Speicherort.cs
+++ Die falsche Insel/Speicherort.cs
@@ -8,6 +8,7 @@
     private int baumstamm;
     private int fliegenpilz;
     private int pilz;
+    private int stein;
     private int stock;
 
 
@@ -15,14 +16,16 @@
     public int Baumstamm { get => baumstamm; set => baumstamm = value;}
     public int Fliegenpilz { get => fliegenpilz; set => fliegenpilz = value;}
     public int Pilz { get => pilz; set => pilz = value;}
+    public int Stein { get =>  stein; set => stein = value;}
     public int Stock { get => stock; set => stock = value;}
 
-    public Speicherort(int energie, int baumstamm, int fliegenpilz, int pilz, int stock)
+    public Speicherort(int energie, int baumstamm, int fliegenpilz, int pilz, int stein, int stock)
     {
         Energie = energie;
         Baumstamm = baumstamm;
         Fliegenpilz = fliegenpilz;
         Pilz = pilz;
+        Stein = stein;
         Stock = stock;
     }
 }
