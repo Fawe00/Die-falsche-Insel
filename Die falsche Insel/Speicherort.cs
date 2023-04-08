6d8a5bcdf98509b518106a9cca5a33f18821fb36
Fawe00 <Fawe1@gmx.net>
2023-04-08 10:18:15 +0200

    Variablen Bauer

--- Die falsche Insel/Speicherort.cs
+++ Die falsche Insel/Speicherort.cs
@@ -21,6 +21,9 @@
     //König
     private bool koenigAugabeErhaeltlich = true;
     private int koenigAugebeStuffe = 0;
+    //Bauer
+    private bool bauerAufgabeErhalten = true;
+    private int bauerAufbabenStuffe = 0;
 
     public int Energie { get => energie; set => energie = value; }
     public int Apfel { get => apfel; set => apfel = value; }
@@ -36,6 +39,8 @@
 
     public bool KoenigAufgabe { get => koenigAugabeErhaeltlich; set => koenigAugabeErhaeltlich = value;}
     public int KoenigAugabenStuffe { get => koenigAugebeStuffe; set => koenigAugebeStuffe = value;}
+    public bool BauerAufgabe { get => bauerAufgabeErhalten; set => bauerAufgabeErhalten = value;}
+    public int BauerAufgabeStuffe { get => bauerAufbabenStuffe; set => bauerAufbabenStuffe = value;}
 
     public Speicherort(int energie,int apfel, int birne, int baumstamm, int fliegenpilz, int karotten, int kartoffeln, int kraut, int pilz, int stein, int stock)
     {
