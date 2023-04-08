a80971fd3a10c4202065141fb2e0c954e46a4618
Fawe00 <Fawe1@gmx.net>
2023-04-08 10:16:45 +0200

    Aufgabe Bauer

--- Die falsche Insel/Story.cs
+++ Die falsche Insel/Story.cs
@@ -53,4 +53,51 @@
             Console.WriteLine("Der König hat keine Aufgabe für Sie.");
         }
     }
+    public void BauerAufgabe()
+    {
+        if (_speicherort.BauerAufgabe)
+        {
+            switch(_speicherort.BauerAufgabeStuffe)
+            {
+                case 0:
+                    Console.WriteLine("Hallo Fremder wie kommt es zu den das Sie hier auf der insel der Ernte?");
+                    Console.WriteLine("Egal wie sie schon hier seit könnt ihr mir ein gefallen helfen?");
+                    Console.WriteLine("Könnte ihr mir bitte 1 Birne und 1 Apfel bringen?");
+                    Console.WriteLine("Ich will eine Pause machen und habe keine Obst");
+                    _speicherort.BauerAufgabeStuffe++;
+                    break;
+                case 1:
+                    Console.WriteLine("Oh sie sind zurück haben sie das Obst?");
+                    eingabe = Console.ReadLine();
+                    if (eingabe != null)
+                    {
+                        switch(eingabe)
+                        {
+                            case "Ja":
+                                Console.WriteLine("Zeigen sie mal her");
+                                if(_speicherort.Apfel >= 1 && _speicherort.Birne >= 1)
+                                {
+                                    Console.WriteLine("Oh wie schön wiklich sehr frisch.");
+                                    _speicherort.Apfel = 0;
+                                    _speicherort.Birne = 0;
+                                    Console.WriteLine("Danke für das Obst.");
+                                    Console.WriteLine("Sie können jederzeit weiters Essen auch für Sie ernten das sie wieder mehr energie haben.");
+                                    Console.WriteLine("Essen können Sie bei der Burg im Speisesall.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Sie haben garkeit oder nur ein Obst gepückt schauen sie nochmals in ihren Bäute nach was sie nicht haben und gehen sie das Ernten.");
+                                }
+                                break;
+                            case "Nein":
+                                Console.WriteLine("Sie können zust zu den Obstbäumen und dann dort Äpfel oder Birnen plücken.");
+                                break;
+                            default:
+                                Console.WriteLine("Sie können bei Aufgaben nur Ja oder Nein schreiben!");
+                                break;
+                        }
+                    }
+            }
+        }
+    }
 }
