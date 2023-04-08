ccaacce1d6bb3aa679154039c623d84b1d2bd7bc
Fawe00 <Fawe1@gmx.net>
2023-04-08 09:10:10 +0200

    Inventar Updaten

--- Die falsche Insel/Inventar.cs
+++ Die falsche Insel/Inventar.cs
@@ -9,8 +9,12 @@
     public void InventarAusagabe()
     {
         Console.WriteLine("Energie:" + _speicherort.Energie);
+        Console.WriteLine("Apfel" + _speicherort.Apfel);
+        Console.WriteLine("Birne" + _speicherort.Birne);
         Console.WriteLine("Baumstamm:" + _speicherort.Baumstamm);
         Console.WriteLine("Fiegenpilz:" + _speicherort.Fliegenpilz);
+        Console.WriteLine("Karotten" + _speicherort.Karotten);
+        Console.WriteLine("Kartoffeln" + _speicherort.Kartoffeln);
         Console.WriteLine("Kräuter" + _speicherort.Kraut);
         Console.WriteLine("Pilz:" + _speicherort.Pilz);
         Console.WriteLine("Stock:" + _speicherort.Stock);
