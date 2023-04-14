2eb1a11caf653a02ef3dbffee15046d5bed6d8c3
Fawe00 <Fawe1@gmx.net>
2023-04-14 23:01:14 +0200

    Erstellen Fluss

---
+++ Die falsche Insel/Fluss.cs
@@ -0,0 +1,20 @@
+﻿class Fluss
+{
+    private String _eingabe;
+    private readonly Verabreitung _verarbeitung;
+    private readonly Inventar _inventar;
+
+    public Fluss(Verabreitung verabreitung, Inventar inventar)
+    {
+        _verarbeitung = verabreitung;
+        _inventar = inventar;
+    }
+    public String FlussMain()
+    {
+        Console.WriteLine("Wikommen auf der Insel des Flusses");
+        while (true)
+        {
+
+        }
+    }
+}
