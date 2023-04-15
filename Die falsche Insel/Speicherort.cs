12fefa11b7fa8ca9a695384067f9245b8c97e350
Fawe00 <Fawe1@gmx.net>
2023-04-15 16:21:02 +0200

    Update Speicherort
    Beutel, Lehm, Sand, WasserBeutel,
    Bool: Brückefluss

--- Die falsche Insel/Speicherort.cs
+++ Die falsche Insel/Speicherort.cs
@@ -8,13 +8,19 @@
     private int apfel;
     private int birne;
     private int baumstamm;
+    private int beutel;
     private int fliegenpilz;
     private int karotten;
     private int kartoffeln;
     private int kraut;
+    private int lehm;
     private int pilz;
+    private int sand;
     private int stein;
     private int stock;
+    private int wasserBeutel;
+    //Bauen
+    private bool brueckeFluss = false;
 
 
     //Story
@@ -29,13 +35,18 @@
     public int Apfel { get => apfel; set => apfel = value; }
     public int Birne { get => birne; set => birne = value; }
     public int Baumstamm { get => baumstamm; set => baumstamm = value;}
+    public int Beutel { get => beutel; set => beutel = value; }
     public int Fliegenpilz { get => fliegenpilz; set => fliegenpilz = value;}
     public int Karotten { get => karotten; set => karotten = value;}
     public int Kartoffeln { get => kartoffeln; set => kartoffeln = value;}
     public int Kraut { get => kraut; set => kraut = value;}
+    public int Lehm { get => lehm; set => lehm = value; }
     public int Pilz { get => pilz; set => pilz = value;}
+    public int Sand { get => sand; set => sand = value; }
     public int Stein { get =>  stein; set => stein = value;}
     public int Stock { get => stock; set => stock = value;}
+    public int WasserBeutel { get => wasserBeutel; set => stock = value; }
+    public bool Brueckefluss { get => brueckeFluss; set => brueckeFluss = value; }
 
     public bool KoenigAufgabe { get => koenigAugabeErhaeltlich; set => koenigAugabeErhaeltlich = value;}
     public int KoenigAugabenStuffe { get => koenigAugebeStuffe; set => koenigAugebeStuffe = value;}
