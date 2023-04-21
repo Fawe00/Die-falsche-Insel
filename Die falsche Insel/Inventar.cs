c77917c9ff82694def7525b4bc6f37ec39d71849
Fawe00 <Fawe1@gmx.net>
2023-04-21 10:31:42 +0200

    Inventar erweitert und verbessert

--- Die falsche Insel/Inventar.cs
+++ Die falsche Insel/Inventar.cs
@@ -9,14 +9,18 @@
     public void InventarAusagabe()
     {
         Console.WriteLine("Energie:" + _speicherort.Energie);
-        Console.WriteLine("Apfel" + _speicherort.Apfel);
-        Console.WriteLine("Birne" + _speicherort.Birne);
+        Console.WriteLine("Apfel: " + _speicherort.Apfel);
+        Console.WriteLine("Birne: " + _speicherort.Birne);
         Console.WriteLine("Baumstamm:" + _speicherort.Baumstamm);
+        Console.WriteLine("Beutel: " + _speicherort.Beutel);
         Console.WriteLine("Fiegenpilz:" + _speicherort.Fliegenpilz);
-        Console.WriteLine("Karotten" + _speicherort.Karotten);
-        Console.WriteLine("Kartoffeln" + _speicherort.Kartoffeln);
-        Console.WriteLine("Kräuter" + _speicherort.Kraut);
+        Console.WriteLine("Karotten: " + _speicherort.Karotten);
+        Console.WriteLine("Kartoffeln: " + _speicherort.Kartoffeln);
+        Console.WriteLine("Kräuter: " + _speicherort.Kraut);
+        Console.WriteLine("Lehm: " + _speicherort.Lehm);
         Console.WriteLine("Pilz:" + _speicherort.Pilz);
+        Console.WriteLine("Sand: " + _speicherort.Sand);
+        Console.WriteLine("Stein: " + _speicherort.Stein);
         Console.WriteLine("Stock:" + _speicherort.Stock);
     }
 }
