73b27ec4d3b3055adedb9e63046485e7c6901aab
Fawe00 <Fawe1@gmx.net>
2023-05-01 21:28:38 +0200

    Wiese Initialisieren

---
+++ Die falsche Insel/Wiese.cs
@@ -0,0 +1,15 @@
+﻿class Wiese
+{
+    private String _eingabe;
+    private readonly Verabreitung _verarbeitung;
+    private readonly Inventar _inventar;
+    private readonly Speicherort _speicherort;
+
+    public Wiese(Verabreitung verarbeitung, Inventar inventar, Speicherort speicherort)
+    {
+        _verarbeitung = verarbeitung;
+        _inventar = inventar;
+        _speicherort = speicherort;
+    }
+
+}
