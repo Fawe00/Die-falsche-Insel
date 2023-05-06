0ab38ee0571a2e7aac8fa2ba28a51c91515f515e
Fawe00 <52331807+Fawe00@users.noreply.github.com>
2023-05-06 20:50:39 +0200

    nochmals Standard Änderung

--- Die falsche Insel/Main.cs
+++ Die falsche Insel/Main.cs
@@ -9,12 +9,12 @@
         Verabreitung verabreitung = new Verabreitung(speicherort);
         Story story = new Story(speicherort);
         Essen essen = new Essen(speicherort);
-        Wald wald = new Wald(verabreitung, inventar);
+        Wald wald = new Wald(verabreitung, inventar, standard);
         Dorf dorf = new Dorf(verabreitung, inventar, standard);
         Burg burg = new Burg(verabreitung, inventar, speicherort, story, essen, standard);
-        Berg berg = new Berg(verabreitung, inventar);
+        Berg berg = new Berg(verabreitung, inventar, standard);
         Bauerhof bauerhof = new Bauerhof(verabreitung, inventar, speicherort, story, standard);
-        Fluss fluss = new Fluss(verabreitung, inventar, speicherort);
+        Fluss fluss = new Fluss(verabreitung, inventar, speicherort, standard);
         Programm programm = new Programm(wald, dorf, burg, berg, bauerhof, fluss);
         programm.Runde();
 
