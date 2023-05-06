0b980e610c21ee0b3cbf248037e8dc6408ba0ef0
Fawe00 <52331807+Fawe00@users.noreply.github.com>
2023-05-06 20:49:35 +0200

    Standard hinzugefügt Klassen Erstellung

--- Die falsche Insel/Main.cs
+++ Die falsche Insel/Main.cs
@@ -3,17 +3,17 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Das Program wird gestartet");
-        Standard standart = new Standard();
+        Standard standard = new Standard();
         Speicherort speicherort = new Speicherort(1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         Inventar inventar = new Inventar(speicherort);
         Verabreitung verabreitung = new Verabreitung(speicherort);
         Story story = new Story(speicherort);
         Essen essen = new Essen(speicherort);
         Wald wald = new Wald(verabreitung, inventar);
-        Dorf dorf = new Dorf(verabreitung, inventar);
-        Burg burg = new Burg(verabreitung, inventar, speicherort, story, essen, standart);
+        Dorf dorf = new Dorf(verabreitung, inventar, standard);
+        Burg burg = new Burg(verabreitung, inventar, speicherort, story, essen, standard);
         Berg berg = new Berg(verabreitung, inventar);
-        Bauerhof bauerhof = new Bauerhof(verabreitung, inventar, speicherort, story, standart);
+        Bauerhof bauerhof = new Bauerhof(verabreitung, inventar, speicherort, story, standard);
         Fluss fluss = new Fluss(verabreitung, inventar, speicherort);
         Programm programm = new Programm(wald, dorf, burg, berg, bauerhof, fluss);
         programm.Runde();
