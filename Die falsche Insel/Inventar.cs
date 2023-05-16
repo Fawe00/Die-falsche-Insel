80f1657c95aba83912336490de466cf9ea667004
Fawe00 <Fawe1@gmx.net>
2023-05-16 18:33:34 +0200

    Inventar Erweitern

--- Die falsche Insel/Inventar.cs
+++ Die falsche Insel/Inventar.cs
@@ -8,20 +8,24 @@
     }
     public void InventarAusagabe()
     {
-        Console.WriteLine("Energie:" + _speicherort.Energie);
+        Console.WriteLine("Energie: " + _speicherort.Energie);
         Console.WriteLine("Apfel: " + _speicherort.Apfel);
         Console.WriteLine("Birne: " + _speicherort.Birne);
-        Console.WriteLine("Bier:" + _speicherort.Bier);
-        Console.WriteLine("Baumstamm:" + _speicherort.Baumstamm);
+        Console.WriteLine("Bier: " + _speicherort.Bier);
+        Console.WriteLine("Baumstamm: " + _speicherort.Baumstamm);
         Console.WriteLine("Beutel: " + _speicherort.Beutel);
-        Console.WriteLine("Fiegenpilz:" + _speicherort.Fliegenpilz);
+        Console.WriteLine("Fiegenpilz: " + _speicherort.Fliegenpilz);
+        Console.WriteLine("Holzscheite: " + _speicherort.Holzscheite);
+        Console.WriteLine("Holzstücke " + _speicherort.Holzstuck);
         Console.WriteLine("Karotten: " + _speicherort.Karotten);
         Console.WriteLine("Kartoffeln: " + _speicherort.Kartoffeln);
         Console.WriteLine("Kräuter: " + _speicherort.Kraut);
         Console.WriteLine("Lehm: " + _speicherort.Lehm);
-        Console.WriteLine("Pilz:" + _speicherort.Pilz);
+        Console.WriteLine("Pilz: " + _speicherort.Pilz);
         Console.WriteLine("Sand: " + _speicherort.Sand);
         Console.WriteLine("Stein: " + _speicherort.Stein);
         Console.WriteLine("Stock:" + _speicherort.Stock);
+        Console.WriteLine("Wasserbäutel " + _speicherort.WasserBeutel);
+        Console.WriteLine("Ziegel: " + _speicherort.Ziegel);
     }
 }
