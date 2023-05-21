47da4488c620ae656d84acf9660a8a828dc59d1a
Fawe00 <52331807+Fawe00@users.noreply.github.com>
2023-05-21 22:29:59 +0200

    Wechsel auf PC

--- Die falsche Insel/Wiese.cs
+++ Die falsche Insel/Wiese.cs
@@ -4,16 +4,39 @@
     private readonly Verarbeitung _verarbeitung;
     private readonly Inventar _inventar;
     private readonly Speicherort _speicherort;
+    private readonly Standard _standard;
 
-    public Wiese(Verarbeitung verarbeitung, Inventar inventar, Speicherort speicherort)
+    public Wiese(Verarbeitung verarbeitung, Inventar inventar, Speicherort speicherort, Standard standard)
     {
         _verarbeitung = verarbeitung;
         _inventar = inventar;
         _speicherort = speicherort;
+        _standard = standard;
     }
     public String WieseMain()
     {
-        // Zuerst korigiren von allen dann weitermachen
         return "Fluss";
+        Console.WriteLine("Willkommen aud der Insel der Wiesen und Freiflächen");
+        while (true)
+        {
+            _standard.StandardInselnWahl();
+            if (_speicherort.SagewerkWiese)
+            {
+                Console.WriteLine("1. Sägewerk betreiben");
+            }
+            else
+            {
+                Console.WriteLine("1. Sägewerk bauen");
+            }
+            if (_speicherort.MuhleWiese)
+            {
+                Console.WriteLine("2. Mühle betreiben");
+            }
+            else
+            {
+                Console.WriteLine("2. Mühle bauen");
+            }
+
+        }
     }
 }
